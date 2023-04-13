a51afef9264a49aeb7e68886a47ced0b0ac61ad8
Alex Meijer <asure007@gmail.com>
2023-04-13 11:13:29 +0200

    First attempt at fixed tables. Python script to search/replace.
    Signed-off-by: Alex Meijer <asure007@gmail.com>

---
+++ fixtbl.py
@@ -0,0 +1,19 @@
+import os
+
+# Define the directory containing the .tbl files
+directory = '.'
+
+# Loop over all .asm files in the directory
+for filename in os.listdir(directory):
+    if filename.endswith('.TBL'):
+        # Open the input file
+        with open(os.path.join(directory, filename), 'r') as file:
+            # Read the content of the file
+            file_content = file.readlines()
+
+        # Remove all lines containing the specified text pattern
+        new_content = [line for line in file_content if ".word   00H" not in line]
+
+        # Write the updated content to the output file
+        with open(os.path.join(directory, filename), 'w') as file:
+            file.writelines(new_content)
